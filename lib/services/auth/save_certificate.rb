4f831ac029ed41e45cfb4b01e085135b4160ebfd
Raul Victor Coan <rvc.coan@gmail.com>
2022-09-26 08:49:56 -0300

    fix certificate file write

--- lib/services/auth/save_certificate.rb
+++ lib/services/auth/save_certificate.rb
@@ -13,20 +13,18 @@ module Services
         def save_certificate(certificate:)
           persist_certificates(certificate: certificate)
           print_warnings
-          p certificate
-          p certificate.to_s
         end
 
         private
 
         def persist_certificates(certificate:)
-          File.new(Dir.pwd) do
-            certificate
+          File::open(File.join([Dir.pwd, "nubank_personal_cert.der"]), 'w+b') do |file|
+            file << certificate.to_der
           end
         end
 
         def print_warnings
-          p 'Certificates generated successfully. (cert.pem)'
+          p 'Certificates generated successfully. (nubank_personal_cert.der)'
           p 'ATTENTION, keep these certificates safe, as they give full access to your account'
         end
       end
